9bb6193538ca86d341f39d8d3aa6bab6aa5d813f
rdtungul <rosephdarl@gmail.com>
2023-11-24 23:27:48 +0800

    mobile menu final

--- js/index.js
+++ js/index.js
@@ -32,49 +32,18 @@ function onTabClick(e) {
     .classList.remove("hidden");
 }
 
-// toggle dark mode
-const themeToggleBtn = document.getElementById("theme-toggle");
-const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+// Hamburger Button Functionality
+// Event Listeners
+btn.addEventListener("click", navToggle());
 
-if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  // Show light icon
-  themeToggleLightIcon.classList.remove("hidden");
-} else {
-  // Show dark icon
-  themeToggleDarkIcon.classList.remove("hidden");
-}
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
 
-// toggle event listeners
-themeToggleBtn.addEventListener("click", toggleMode);
-
-function toggleMode(e) {
-  e.preventDefault();
-  themeToggleDarkIcon.classList.toggle("hidden");
-  themeToggleLightIcon.classList.toggle("hidden");
-
-  // save in the LocalStorage
-  if (localStorage.getItem("color-theme")) {
-    // if it's in light mode, save in the localStorage
-    if (localStorage.getItem("color-theme") === "light") {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    }
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "../images/logo-bookmark-footer.svg");
   } else {
-    // if not in the localStorage
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    }
+    logo.setAttribute("src", "../images/logo-bookmark.svg");
   }
 }
